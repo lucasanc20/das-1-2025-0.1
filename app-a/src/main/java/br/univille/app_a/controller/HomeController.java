be15173d8d8739f11fa2c4f1d225ac99c9cacb21
lucasanc20 <110943898+lucasanc20@users.noreply.github.com>
2025-06-26 00:15:28 +0000

    comitar né meu

---
+++ app-a/src/main/java/br/univille/app_a/controller/HomeController.java
@@ -0,0 +1,33 @@
+package br.univille.app_a.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import io.dapr.client.DaprClient;
+import io.dapr.client.DaprClientBuilder;
+import io.dapr.client.domain.HttpExtension;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping("/api")
+public class HomeController {
+
+    //  ******** APPLICAÇÃO AAAAAAAAA  ************
+
+    @GetMapping()
+    public ResponseEntity index() {
+
+        try(DaprClient daprClient = new DaprClientBuilder().build()){
+         
+            daprClient.invokeMethod("app_b", "/api", null, HttpExtension.GET).block();
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+
+        return ResponseEntity.ok().body("Hello from App A");
+    }
+}
